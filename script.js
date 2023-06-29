318fa34873ce15ceb0ed223b5438a9b07d6201cb
Thiago José <thiago.jose32@outlook.com>
2023-06-29 17:16:59 -0300

    Updates

---
+++ script.js
@@ -0,0 +1,9 @@
+function mouseDown() {
+    var img = document.getElementById('iconCart');
+    img.setAttribute('src', 'media/icon-CarrinhoPressed.png')
+}
+
+function mouseUp() {
+    var img = document.getElementById('iconCart');
+    img.setAttribute('src', 'media/icon-Carrinho.png')
+}
